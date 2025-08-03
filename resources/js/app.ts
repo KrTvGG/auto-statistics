4889b5c2548b31bfb8b9d3867863150351ef1ddb
KrTvGG <vanyusha.krutov2@bk.ru>
2025-08-03 21:29:00 +0300

    тест подключения scss (неудачный)

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,4 +1,5 @@
-import '../css/app.css';
+// import '../css/app.css';
+import '../scss/runtime/modules.scss';
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
---
+++ resources/js/components/MainMenu.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="text">
+            test
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .text {
+        font-size: clampFluid(200);
+        color: red;
+    }
+</style>
