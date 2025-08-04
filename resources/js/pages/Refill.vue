1f8f81e9ad652ae31f7d5f86f2c5fe4a7203d5c0
KrTvGG <vanyusha.krutov2@bk.ru>
2025-08-04 20:22:58 +0300

    реализован основной контроллер и страница с заправкой

---
+++ resources/js/pages/Refill.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+    import {
+        Link
+    } from '@inertiajs/vue3'
+</script>
+
+<template>
+    <Link href="/">Назад</Link>
+    <br>
+    Заправка
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\MainController;
 
 Route::get('/', function () {
     return Inertia::render('index');
 })->name('home');
+
+Route::get('/refill', [MainController::class, 'indexRefill'])->name('refill.index');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
